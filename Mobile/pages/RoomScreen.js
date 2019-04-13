68d0f3e45a5413a1b3d6df669251ec235b5e51f0
Ayo <ayojohnson3@gmail.com>
2019-04-12 23:12:58 -0400

    Back Icon for Image

--- Mobile/pages/RoomScreen.js
+++ Mobile/pages/RoomScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from '../components/Header'
 
 export default class RoomScreen extends React.Component {
 
   render() {
     return (
       <View style={{ backgroundColor: 'white', flex: 1 }}>
+        <Header
+          titel="Shoot"
+        />
+
         <Text>Room Screen</Text>
       </View>
     );
