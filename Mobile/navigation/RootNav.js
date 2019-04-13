ecba6cc4247526bb843ed5ef6d84d50b1aa357a6
David Margolin <davidmargolin0305@gmail.com>
2019-04-12 22:51:40 -0400

    first pages

---
+++ Mobile/navigation/RootNav.js
@@ -0,0 +1,21 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from '../pages/HomeScreen';
+import RoomScreen from '../pages/RoomScreen';
+import ShootScreen from '../pages/ShootScreen';
+
+
+export default createAppContainer(createStackNavigator({
+    Home: {
+        screen: HomeScreen
+    },
+    Room: {
+        screen: RoomScreen
+    },
+    Shoot: {
+        screen: ShootScreen
+    }
+},
+    {
+        initialRouteName: 'Home',
+        headerMode: 'none'
+    }));
